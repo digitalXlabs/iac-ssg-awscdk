fcb40e736705c288db5e15e33e59f15311e668a3
Morlene Fisher <morlene@ashleylawrence.com>
2021-06-18 13:44:01 +0100

    Added switch to differentiate production from non production stack

--- test/ssg-template.test.ts
+++ test/ssg-template.test.ts
@@ -3,36 +3,82 @@ import '@aws-cdk/assert/jest';
 import * as cdk from '@aws-cdk/core';
 import * as SSGTemplate from '../lib/ssg-stack';
 
-test('Resources test', () => {
-    const app = new cdk.App();
-    // WHEN
-    const stack = new SSGTemplate.SSGStack(app, 'MyTestStack', {
-      stackName: "examplestackname",
-      env: {
-        account: "testaccount1",
-        region: "eu-west-1"
-      },
-       description: "test description",
-       owner: "ExampleOwner",
-       ownerId: "ExampleClientId",
-       stackPrefix: "ExampleStack",
-       fqdn: "example.com",
-       isProd: false
-    });
+test('Resources non production test', () => {
+  const app = new cdk.App();
+  // WHEN
+  const stack = new SSGTemplate.SSGStack(app, 'MyTestStack', {
+    stackName: "examplestackname",
+    env: {
+      account: "testaccount1",
+      region: "eu-west-1"
+    },
+    description: "test description",
+    owner: "ExampleOwner",
+    ownerId: "ExampleClientId",
+    stackPrefix: "ExampleStack",
+    fqdn: "example.com",
+    isProd: false
+  });
 
 
-    expect(stack).toHaveResource('AWS::S3::Bucket');
-    expect(stack).toHaveResource('AWS::S3::BucketPolicy');
-    expect(stack).toHaveResource('Custom::S3AutoDeleteObjects');
-    expect(stack).toHaveResource('AWS::IAM::Role');
-    expect(stack).toHaveResource('AWS::Lambda::Function');
+  expect(stack).toHaveResource('AWS::S3::Bucket', {
+    "BucketName": "examplestacks3bucket",
+    "VersioningConfiguration": {
+      "Status": "Enabled"
+    }
+  });
+  expect(stack).toHaveResource('AWS::S3::BucketPolicy');
+  expect(stack).toHaveResource('Custom::S3AutoDeleteObjects');
+  expect(stack).toHaveResource('AWS::IAM::Role');
+  expect(stack).toHaveResource('AWS::Lambda::Function');
+  expect(stack).toHaveResource('AWS::IAM::Policy');
+  expect(stack).toHaveResource('AWS::CloudFormation::CustomResource');
+  expect(stack).toHaveResource('Custom::CrossRegionStringParameterReader');
+  expect(stack).toHaveResource('AWS::CloudFront::CloudFrontOriginAccessIdentity');
+  expect(stack).toHaveResource('AWS::CloudFront::Distribution');
+  expect(stack).toHaveResource('AWS::Route53::RecordSet', {
+    "Name": "stage.example.com.",
+    "Type": "A"
+  });
 
-    console.log(stack);
-    // expect(stack).toHaveOutput({'outputName': 'S3Bucket'})
-    // expect(stack).toHaveOutput({'outputName': 'Certificate'})
-    // expect(stack).toHaveOutput({'outputName': 'CloudfrontDistrubutionID'})
-    // expect(stack).toHaveOutput({'outputName': 'RecordSetDomain'})
-    // expect(stack).toHaveOutput({'outputName': 'LambdaEdgeFunctionArn'})
-    // expect(stack).toHaveOutput({'outputName': 'LambdaEdgeFunctionVersion'})
-    
+  expect(stack).toHaveOutput({ "outputName": "bucket" });
+  expect(stack).toHaveOutput({ "outputName": "distro" });
+  expect(stack).toHaveOutput({ "outputName": "certificate" });
+  expect(stack).toHaveOutput({ "outputName": "function" });
+  expect(stack).toHaveOutput({ "outputName": "functionversion" });
+
+
+
+});
+
+
+test('Resources production test', () => {
+  const app = new cdk.App();
+  // WHEN
+  const stack = new SSGTemplate.SSGStack(app, 'MyTestStack', {
+    stackName: "examplestacknameproduction",
+    env: {
+      account: "testaccount1",
+      region: "eu-west-1"
+    },
+    description: "test description",
+    owner: "ExampleOwner",
+    ownerId: "ExampleClientId",
+    stackPrefix: "ExampleStackProduction",
+    fqdn: "example.com",
+    isProd: true
+  });
+
+  expect(stack).toHaveResource('AWS::S3::Bucket', {
+    "BucketName": "examplestackproductions3bucket",
+    "VersioningConfiguration": {
+      "Status": "Enabled"
+    }
+  });
+
+  expect(stack).toHaveResource('AWS::Route53::RecordSet', {
+    "Name": "prod.example.com.",
+    "Type": "A"
+  });
+
 });
